e56fc88d3eba272009be7aaf028b60da412f14ab
Ana Andres-Arroyo <anaandresarroyo@gmail.com>
2020-12-06 15:25:12 +0000

    Add convert_fit_to_csv()

--- src/fitanalyser/fitparser.py
+++ src/fitanalyser/fitparser.py
@@ -1,16 +1,25 @@
+from pathlib import Path
+from pyprojroot import here
 import pandas as pd
+from fitparse import FitFile
 
 
 def get_records(fitfile):
-    """
-    Get the timeseries records from the FitFile object.
+    """Get the timeseries records from the FitFile object.
 
-    :param fitfile: (fitparse FitFile)
-    :return: record: (pandas DataFrame)
+    Parameters
+    ----------
+    fitfile: (fitparse FitFile)
+        The object containing the activity data from a Garmin device.
+    
+    Returns
+    -------
+    record: (pandas DataFrame)
+        The contents of the data in a tabular format.
     """
     # Get timeseries 'record' data from the FitFile object
     records = pd.DataFrame()
-    for im, message in enumerate(fitfile.get_messages('record')):
+    for _, message in enumerate(fitfile.get_messages('record')):
         data = {message_data.name: message_data.value for message_data in message}
         records = records.append(pd.Series(data), ignore_index=True)
 
@@ -26,3 +35,37 @@ def get_records(fitfile):
     records = records.sort_index()
 
     return records
+
+
+def convert_fit_to_csv(fit_file_path, csv_file_path):
+    """Read the activity data from a .fit file and save it as a .csv file.
+
+    Parameters:
+    -----------
+    fit_file_path: (path or str)
+        Should point to a .fit file that already exists.
+
+    csv_file_path: (path or str)
+        Should point to a .csv file. 
+        If the file does not yet exist, it will be created.
+        If the file already exists, it will be overwritten.
+    """
+    fitfile = FitFile(str(fit_file_path))
+    records = get_records(fitfile)
+    records.to_csv(csv_file_path)
+
+
+if __name__ == "__main__":
+
+    # Get the location of the .fit and .csv files.
+    project_root = here()
+    fit_dir = Path(project_root / 'data' / 'fit')
+    csv_dir = Path(project_root / 'data' / 'csv')
+
+    # All items in fit_dir should be .fit files.
+    for fit_file_path in fit_dir.iterdir():
+        csv_file_path = csv_dir / fit_file_path.name.replace('.fit', '.csv')
+        # Only convert the file if the .csv file does not already exist.
+        if not csv_file_path.is_file():
+            print('Converting file: %s' % fit_file_path.name)
+            convert_fit_to_csv(fit_file_path, csv_file_path)
